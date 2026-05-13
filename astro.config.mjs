c01b59bc6af1a57e9680165e97a94e6fd00fccde
Samuel Bonifacio <samuelbonifacio015@gmail.com>
2026-05-12 22:32:43 -0500

    fix: change astro adapter to vercel for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react()],
 });
